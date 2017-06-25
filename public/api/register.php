50ab644cc734934f3e592936e7bd6d817b1e2298
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2017-06-25 18:34:54 -0400

    Expanded config array, added config checks

--- public/api/register.php
+++ public/api/register.php
@@ -18,75 +18,151 @@ require_once "../../common/functions.php";
 $acceptableFields = [
     "f_name" => [
         "filter" => FILTER_SANITIZE_STRING,
-        "name" => "First Name"
+        "name" => "First Name",
+        "length" => [
+            "min" => 2,
+            "max" => 50
+        ]
     ],
     "l_name" => [
         "filter" => FILTER_SANITIZE_STRING,
-        "name" => "Last Name"
+        "name" => "Last Name",
+        "length" => [
+            "min" => 2,
+            "max" => 50
+        ]
     ],
     "email" => [
         "filter" => FILTER_SANITIZE_EMAIL,
-        "name" => "E-Mail"
+        "name" => "E-Mail",
+        "length" => [
+            "min" => 7,
+            "max" => 255
+        ]
     ],
     "pass" => [
         "filter" => FILTER_DEFAULT, //Pass gets hashed, so no real issue of injection here
-        "name" => "Password"
+        "name" => "Password",
+        "length" => [
+            "min" => 8,
+            "max" => 255
+        ]
     ],
     "age" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "Age"
+        "name" => "Age",
+        "length" => [
+            "min" => 2,
+            "max" => 2
+        ],
+        "value" => [
+            "min" => 15,
+            "max" => 99
+        ]
     ],
     "gender" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "Gender"
+        "name" => "Gender",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //Normalize - DONE
     "class_year" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "Class Year"
+        "name" => "Class Year",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //Normalize - DONE
     "school" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "School"
+        "name" => "School",
+        "length" => [
+            "min" => 1,
+            "max" => 3
+        ]
     ], //Normalize - DONE
     "race" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "Race"
+        "name" => "Race",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //normalize - DONE
     "is_hispanic" => [
         "filter" => FILTER_VALIDATE_BOOLEAN,
-        "name" => "Are you of Hispanic/Latino origins?"
+        "name" => "Are you of Hispanic/Latino origins?",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //Boolean is hispanic
     "zip_code" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "City"
+        "name" => "City",
+        "length" => [
+            "min" => 5,
+            "max" => 5
+        ]
     ],
     "state" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "State"
+        "name" => "State",
+        "length" => [
+            "min" => 1,
+            "max" => 2
+        ]
     ], //Normalize - DONE
     "shirt_size" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "Shirt Size"
+        "name" => "Shirt Size",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //Normalize - DONE
     "diet_restrictions" => [
         "filter" => FILTER_SANITIZE_NUMBER_INT,
-        "name" => "Dietary Restrictions"
+        "name" => "Dietary Restrictions",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //Normalize - IN PROGRESS
     "diet_other" => [
         "filter" => FILTER_SANITIZE_STRING,
-        "name" => "Diet Other"
+        "name" => "Diet Other",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ],
     "github" => [
         "filter" => FILTER_SANITIZE_STRING,
-        "name" => "GitHub Profile"
+        "name" => "GitHub Profile",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //URL Escape and only the username
     "linkedin" => [
         "filter" => FILTER_SANITIZE_STRING,
-        "name" => "LinkedIn Profile"
+        "name" => "LinkedIn Profile",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ], //Ditto
     "is_first_hackathon" => [
         "filter" => FILTER_VALIDATE_BOOLEAN,
-        "name" => "Is this your first hackathon?"
+        "name" => "Is this your first hackathon?",
+        "length" => [
+            "min" => 1,
+            "max" => 1
+        ]
     ],
     "resume" => [
         "name" => "Resume"
@@ -146,7 +222,22 @@ header("Content-Type: application/json");
 //Create array to push error messages to
 $errors = [];
 
-//First check captcha
+//Check if we're NOT accepting registrations OR walk-ins
+/*
+if(!($site->isAcceptingRegistrations() OR $site->isAcceptingWalkinIns())){
+    http_response_code(400);
+    $errors[] = "Sorry, registrations are currently closed.";
+}
+
+//We don't accept resumes from walk-ins
+if($site->isAcceptingWalkIns()){
+    if(($key = array_search("resume", $requiredFields)) !== false) {
+        unset($requiredFields[$key]);
+    }
+}
+*/
+
+//Check captcha
 //$response = $_POST['g-recaptcha-response'];
 
 /*if(!recaptcha_verify($response)){
@@ -182,9 +273,39 @@ if(count($missing) > 0){
 
     //Check which specific required fields are missing
     foreach($missing as $field){
-        $errors[] = "Field '" . $field['name'] . "' is missing.";
+        $errors['Missing Fields'][] = "Field '" . $field['name'] . "' is missing.";
     }
 
+    //Output the response
+    json_response($errors);
+}
+
+//By now we have all our required fields, now we need to make sure all fields are following length and value checks
+foreach ($_POST as $key => $value){
+    $fieldName = $acceptableFields[$key]['name'];
+    if(isset($acceptableFields[$key]['length'])){
+        //We have to check the length of applicable values, even numbers
+        $valueLength = strlen($value);
+        $min = $acceptableFields[$key]['length']['min'];
+        $max = $acceptableFields[$key]['length']['min'];
+        if($valueLength < $min)
+            $errors['Value Length'] = "Length of field '$fieldName' is less than the minimum of '$min'";
+        if($valueLength > $max)
+            $errors['Value Length'] = "Length of field '$fieldName' is greater than the maximum of '$max'";
+    }
+
+    //Value is used for checking if the actual value of a field is within a specified range
+    if(isset($acceptableFields[$key]['value'])){
+        $min = $acceptableFields[$key]['value']['min'];
+        $max = $acceptableFields[$key]['value']['min'];
+        if($value < $min)
+            $errors['Value Size'] = "Value of field '$fieldName' is less than the minimum of '$min'";
+        if($value > $max)
+            $errors['Value Size'] = "Value of field '$fieldName' is greater than the maximum of '$max'";
+    }
+}
+//Check if there are any errors so far
+if(!empty($errors))
     json_response($errors);
 
-}
+
