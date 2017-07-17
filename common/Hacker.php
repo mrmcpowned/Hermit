c0fc187449b49d37ad25b6237cd70c2f07a46637
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2017-07-17 01:39:06 -0400

    Removed login method, as it was reworked into an endpoint

--- common/Hacker.php
+++ common/Hacker.php
@@ -58,59 +58,7 @@ class Hacker extends User
         return $this->userInfo['l_name'];
     }
 
-    /**
-     * Login method
-     * @param $email string User's email
-     * @param $pass string User's password
-     * @return mixed If the action succeeded or not
-     */
-    //TODO: Rework this function so it can be implemented better with a login endpoint
-    //NOTE: Probably better that a 'User' doesn't handle login, since it doesn't allow
-    //for error handling very well.
-    //From here we assume user input has been sanitized
-    public function login($email, $pass)
-    {
-        $sql = "SELECT * FROM hackers WHERE email=:email LIMIT 1";
-
-        //Every attempt at login should effectively regenerate the ID, since it's an attempt at privilege elevation
-        session_regenerate_id(true);
-
-        try {
-            $query = $this->db->prepare($sql);
-            $query->bindParam(":email", $email);
-            $query->execute();
-
-            $result = $query->fetch();
-
-            //Result is only false on no match
-            if (!$result) {
-                //TODO: Log incorrect attempt
-                return ['error' => "Email does not exist"];
-            }
-            //Login failed due to incorrect
-            if (!password_verify($pass, $result['pass'])) {
-                //TODO: Log incorrect attempt
-                return ['error' => "Password is incorrect"];
-            }
-
-            $sql = "UPDATE hackers SET sid = :session WHERE id=:user";
-            $userSID = generateSID();
-            $query = $this->db->prepare($sql);
-            $query->bindParam(":user", $result['id']);
-            $query->bindParam(":session", $userSID);
-            $query->execute();
-
-            $this->setSID($userSID);
-            $this->extendSession();
-            return true;
-
-        } catch (PDOException $e) {
-            //If we've reached this stage, chances are I won't be able to log this unless it's in a flat file
-            return ['error' => $e->getMessage()];
-        }
-
-
-    }
+    //DONE: Rework this function so it can be implemented better with a login endpoint
 
     /**
      * Logs out the currently logged in user
