1f377bf6b5502a8251cf42fc08a3b8c8a273b4f5
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2017-07-17 01:58:53 -0400

    For future security reason, only a template will be comitted

--- common/config-template.php
+++ common/config-template.php
@@ -5,8 +5,6 @@ define("DB_USER", "hermit");
 define("DB_PASS", "9LGmAIdbWi6KgmWK");
 define("DB_NAME", "test");
 
-define("RECAPTCHA_SECRET", "6LcIOikUAAAAAKOqTEqRnC-S1O2W-3m2vBISyzhq");
-
 define('SESSION_EXPIRATION_SECONDS', 3600); //1 Hour Expiration
 
 
