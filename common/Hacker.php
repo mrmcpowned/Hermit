b36dd86d616306399006c0579fcb52897d22e118
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2017-07-19 01:39:54 -0400

    Moved setup logic to constructor, added session check to userSetup

--- common/Hacker.php
+++ common/Hacker.php
@@ -18,19 +18,19 @@ class Hacker extends User
     {
         $this->db = $dbPDO;
         $this->sidType = "hacker-sid";
-        $this->userSetup();
+        if ($this->isLoggedIn())
+            $this->userSetup();
         $this->sessionCheck();
         $this->extendSession();
+
+
     }
 
     /**
      * Sets up a logged in user by storing user details in an associative array
      */
-    public function userSetup()
+    private function userSetup()
     {
-        if (!$this->isLoggedIn())
-            return;
-
         $sql = "SELECT * FROM hackers WHERE sid = :sid";
 
         $query = $this->db->prepare($sql);
@@ -40,9 +40,15 @@ class Hacker extends User
 
         $this->userInfo = $query->fetch(PDO::FETCH_ASSOC);
 
+        //If the SID On the server changed, our fetch came up false
+        if($this->userInfo === false){
+            $this->logout();
+            return;
+        }
+
+        $this->passHash = $this->userInfo['pass'];
         //We have to do some cleanup here so we don't accidentally expose any unwanted columns
         unset($this->userInfo['pass']);
-        $this->passHash = $this->userInfo['id'];
         unset($this->userInfo['id']);
         unset($this->userInfo['sid']);
 
@@ -58,18 +64,16 @@ class Hacker extends User
         return $this->userInfo['l_name'];
     }
 
-    //DONE: Rework this function so it can be implemented better with a login endpoint
-
     /**
      * Logs out the currently logged in user
+     *
+     * TODO: Possibly add a message parameter so a messaage could be left in the new session explaining what kind of
+     * logout occurred.
      *
      * @return bool
      */
     public function logout()
     {
-        if (!$this->isLoggedIn())
-            return false;
-
         $sid = $this->getSID();
         $sql = "UPDATE hackers SET sid = NULL, current_ip = NULL WHERE sid=:usersid";
         $query = $this->db->prepare($sql);
@@ -77,6 +81,6 @@ class Hacker extends User
         $query->execute();
         $this->destroySession();
         session_start();
-        return true;
     }
+
 }
