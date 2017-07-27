bccdf32f71fcab217a5f97c1bb6327f38711ffc3
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2017-07-26 23:53:36 -0400

    Added new access points for future pages on the site

---
+++ public/dash/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: mrmcp
+ * Date: 7/26/2017
+ * Time: 10:38 PM
+ */
---
+++ public/dash/me.php
@@ -0,0 +1,4 @@
+<?php
+/**
+ * This page displays info about the currently logged in hacker
+ */
