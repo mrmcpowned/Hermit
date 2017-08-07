db8e562d5d8c08bde930e1ff1b3d64bc9f8033a1
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2017-08-06 21:01:29 -0400

    Added a path constant for the templates folder

--- common/constants-template.php
+++ common/constants-template.php
@@ -15,3 +15,4 @@ define("RECAPTCHA_SECRET", "");
 define('SESSION_EXPIRATION_SECONDS', 3600); //1 Hour Expiration
 define('RESUME_PATH', __DIR__ . "/resumes/"); //Relative to the constant file, but absolute when used
 define('EMAIL_TEMPLATES_PATH', __DIR__ . "/../templates/email");
+define('PUBLIC_TEMPLATES_PATH', __DIR__ . "/../templates/public");
