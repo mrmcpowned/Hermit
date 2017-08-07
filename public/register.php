366b1b9ad5cc4390faf081407d95086b879044c8
Christopher Rodriguez <mrmcpowned@users.noreply.github.com>
2017-08-06 21:12:01 -0400

    Created register controller with basic logic

---
+++ public/register.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../common/config.php';
+/**
+ * Created by PhpStorm.
+ * User: mrmcp
+ * Date: 8/5/2017
+ * Time: 8:28 PM
+ */
+$user = new Hacker($db);
+$loader = new Twig_Loader_Filesystem(PUBLIC_TEMPLATES_PATH);
+$twig = new Twig_Environment($loader);
+
+$context = [
+    "site" => $site,
+    "user" => $user
+];
+
+$template = "register.html.twig";
+
+echo $twig->render($template, $context);
